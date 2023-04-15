9ac2d87a5a9723d9678ff8d973bc9b1ddd434d84
motsu8e <motsu159357@gmail.com>
2023-04-15 21:02:42 +0900

    テトラミノ作成

--- src/index.js
+++ src/index.js
@@ -9,8 +9,22 @@ canvas.height = canvasSizeY;
 canvas.style.border = "3px solid #555";
 var gameBoard = canvas.getContext("2d");
 gameBoard.lineWidth = 10;
-gameBoard.fillStyle = "black";
-gameBoard.fillRect(0, 30, blockSize, blockSize);
+var mino = [
+    [0, 1, 0, 0],
+    [0, 1, 0, 0],
+    [0, 1, 0, 0],
+    [0, 1, 0, 0]
+];
+for (var i = 0; i < mino.length; i++) {
+    for (var j = 0; j < 4; j++) {
+        if (mino[i][j]) {
+            var px = j * blockSize;
+            var py = i * blockSize;
+            gameBoard.fillStyle = "red";
+            gameBoard.fillRect(px, py, blockSize, blockSize);
+        }
+    }
+}
 var total = 0;
 var score = document.getElementById("score");
 score.innerHTML = "score: ".concat(total);
--- src/index.ts
+++ src/index.ts
@@ -11,8 +11,24 @@ canvas.style.border = "3px solid #555"
 
 const gameBoard: CanvasRenderingContext2D = <CanvasRenderingContext2D>canvas.getContext("2d");
 gameBoard.lineWidth = 10
-gameBoard.fillStyle = "black"
-gameBoard.fillRect(0,0,blockSize, blockSize)
+
+const mino: Array<Array<number>> = [
+    [0,1,0,0],
+    [0,1,0,0],
+    [0,1,0,0],
+    [0,1,0,0]
+]
+
+for(let i = 0; i < mino.length; i++){
+    for(let j = 0; j < 4; j++){
+        if(mino[i][j]){
+            let px = j * blockSize
+            let py = i * blockSize
+            gameBoard.fillStyle = "red";
+            gameBoard.fillRect(px,py, blockSize, blockSize);
+        }
+    }
+}
 
 const total: number = 0;
 
